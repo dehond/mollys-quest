f74b815baaf6f982d337f46056aa3f5ac3559ee5
Julius de Hond <dehond@mit.edu>
2021-11-14 11:46:52 -0500

    Add show transition to dialogbox.

--- main.js
+++ main.js
@@ -6,13 +6,6 @@
 
 var Typewriter = require('typewriter-effect/dist/core');
 
-// let dialogtxt = document.getElementById("dialogtext");
-
-// let typewriter = new Typewriter(dialogtxt, {
-//     loop: false,
-//     delay: 0,
-// });
-
 class Dialog {
     constructor() {
         this.dialogtxt = document.getElementById("dialogtext");
@@ -62,7 +55,18 @@ class Dialog {
         this.dialogbox.style.animation = "animate-hide 3s steps(10, jump-none) both";
         this.dialogbox.addEventListener("animationend", function() {
             _this.dialogbox.style.display = "none";
+            _this.dialogbox.style.animation = "animate-show 3s steps(10, jump-none) both";
         });
+    }
+    showDialogBox() {
+        let _this = this;
+        this.dialogbox.offsetHeight; // Reflow for restarting animation
+        this.dialogbox.style.display = "block";
+        
+        this.dialogbox.addEventListener("animationend", function() {
+            _this.dialogbox.style.display = "block";
+        });
+
     }
     next() {
         if (this.messages.length > 0) {
@@ -74,23 +78,6 @@ class Dialog {
     }
 }
 
-// function blinkPointer() {
-//     if (currentMessage == 2)
-//         {spotlight.showSpotlight()}
-//     else if (currentMessage == 3)   
-//         {molly.runTo(-100, molly.position[1])};
-//     document.getElementsByClassName("Typewriter__cursor")[0].style.display = "none";
-//     let el = document.getElementById("dialognav");
-//     el.style.visibility = "visible";
-// }
-
-// function hidePointer() {
-//     document.getElementsByClassName("Typewriter__cursor")[0].style.display = "";
-//     let el = document.getElementById("dialognav");
-//     el.style.visibility = "hidden";
-// }
-
-// let currentMessage = 0;
 let messages = [`Er was eens een mannetje, in het midden van het land,<br>
                 Die zich niet altijd gedroeg; wie had hem in de hand?<br>
                 Een scheetje, een boertje, voor niemand een genotje,<br>
@@ -111,37 +98,9 @@ let messages = [`Er was eens een mannetje, in het midden van het land,<br>
                 Een soepele worp in de richting van het bos,<br>
                 En Molly rende er achteraan, haar tandjes waren los.
                 `
-            ]
+            ];
 
 let actions = [null, null, () => spotlight.showSpotlight(), () => molly.runTo(-100, molly.position[1])];
-
-// typewriter
-//     .typeString(messages[0])
-//     .start()
-//     .callFunction(blinkPointer);
-
-// function nextDialog() {
-//     hidePointer();
-//     currentMessage += 1;
-//     document.getElementsByClassName("Typewriter__wrapper")[0].innerHTML = "";
-//     typewriter
-//         .typeString(messages[currentMessage])
-//         .start()
-//         .callFunction(blinkPointer);
-
-//     if (currentMessage >= messages.length) {
-//         let el = document.getElementById("dialogBox");
-//         el.innerHTML = "";
-//         el.style.animation = "animate-hide 3s steps(10, jump-none) both";
-//         el.addEventListener("animationend", function() {
-//             el.style.display = "none";
-//         });
-//         startLevel(1);
-//     }
-// }
-
-// window.nextDialog = nextDialog;
-
 let dialog = new Dialog();
 dialog.displayMessages(messages, actions);
 
